51fc042b459516811b425e34b631015c74ec7631
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-27 22:45:09 +0600

    some bug fixed

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -45,7 +45,7 @@ const AuthProvider = ({ children }) => {
                     email: currentUser.email
                 }
 
-                fetch('http://localhost:5000/jwt', {
+                fetch('https://car-doctor-server-chi-rose.vercel.app/jwt', {
                     method: 'POST',
                     headers: {
                         'content-type': 'application/json'
